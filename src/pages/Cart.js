cf2f1088c1c284520a0daa3a8ef0b73ca9b87e47
ys0222 <ggdbstjd0222@gmail.com>
2022-09-23 21:29:57 +0900

    0923

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -53,13 +53,13 @@ ${(props) =>
     props.Img &&
     css`
         display: inline-block;
-        width : 33%;
+        width : 20%;
     `}
 ${(props) =>
     props.Name &&
     css`
         display: inline-block;
-        width : 33%;
+        width : 40%;
         text-align : center;
         vertical-align : top;
     `}    
@@ -67,7 +67,7 @@ ${(props) =>
     props.Price &&
     css`
         display: inline-block;
-        width : 33%;
+        width : 40%;
         text-align : center;
         vertical-align : top;
 
@@ -79,16 +79,72 @@ height: 70px;
 `
 const Name = styled.span`
 font-size: 0.85rem;
+${(props) =>
+    props.Price &&
+    css`
+        width : 100%;
+        display: block;
+    `}    
+`
+const Button = styled.button`
+
+background-color: black;
+color : white;
+${(props) =>
+    props.딜리트 &&
+    css`
+        display: inline-block;
+        background-color: aliceblue;
+        border: 2px solid blue;
+        border-radius: 5px;
+        color : black;
+    `} 
+
+
 `
 
 const Hr = styled.hr`
 border-top : 1px dotted #bbb;
+`
+
+const Hr1 = styled.hr`
+border-top : 0.1px dotted #bbb;
 `
 const Result = styled.span`
 font-weight : 1000;
 `
 
 function CartList({item}){
+
+    const [amount,setAmount] = useState(1)
+
+    const plus = (item) =>{
+        setAmount(amount => amount +1)
+        setPrice(price => price += parseInt(item.price))
+    }
+    const minus = () =>{
+        amount > 1 ?
+        setAmount(amount => amount - 1) : setAmount(1)
+
+        price > item.price ?
+        setPrice(price => price -= parseInt(item.price)) : setPrice(parseInt(item.price))
+
+    }
+
+    const [price ,setPrice] = useState(parseInt(item.price))
+
+    const [total,setTotal] = useState(0)
+    useEffect(()=>{
+        setTotal(total => total += price)
+        console.log(total)
+    },[])
+    
+    useEffect(()=>{
+        setTotal(total => total += price)
+        console.log('price',price)
+        console.log(total)
+    },[price])
+
     return (
         <>
             <CartListTableName Img>
@@ -98,9 +154,16 @@ function CartList({item}){
                 <Name>{item.name}</Name>
             </CartListTableName>
             <CartListTableName Price>
-                <Name>{item.price}</Name>
+                <Name Price>{price}원 </Name>
+                <Button 플러스 onClick={()=>plus(item)}> + </Button>
+                <Name>{amount}개 </Name>
+                <Button onClick={()=>minus()}> - </Button>
+                <Hr1 />
+                <Button 딜리트 onClick={()=>minus()}> 삭제 </Button>
             </CartListTableName>
+
             <Hr />
+
         </>
     )
 }
@@ -108,14 +171,6 @@ function CartList({item}){
 
 function Cart(){
     
-    const [result , setResult] = useState(0)
-    
-    Dog.dog.map((item) =>
-    useEffect(()=>
-        setResult(result += item.price)
-        ,[item])
-    )
-
     return(
         <CartContainer>
             <Nav />
@@ -142,13 +197,12 @@ function Cart(){
 
                     <CartTable>
                         <Result>
-                            값 : {result}
+                            총 결제 금액 : 
                         </Result>
                     </CartTable>
 
+
                 </CartWrapper>
-                
-                
         </CartContainer>
     )
 }
